362699edabe3770ab72f5777936684830638358c
seana <seanagius2@gmail.com>
2020-12-12 15:30:23 +0100

    ConnectThree

--- app/src/main/java/com/example/seana/connectthreeotherfinal/MainActivity.java
+++ app/src/main/java/com/example/seana/connectthreeotherfinal/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    // so first we will create general variables to rep an active player, gamestate, winning state, and the number of plays and if the game is active
+    // soo first we will create general variables to rep an active player, gamestate, winning state, and the number of plays and if the game is active
     boolean gameActive = true;
     int currentPlayer = 0;
     int [][] winningStates = {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
